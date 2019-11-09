458b0883c95c2d72a4fc3619ebef8046dc9608e3
ArthurLimaS <als.arthurlimasilva@gmail.com>
2019-11-09 11:36:18 -0300

    Criação das Entidades Básicas
    Criação das classes básicas do sistema (beans). Essas classes não possuem métodos criados, apenas atributos

---
+++ MightySheet/src/negocio/beans/Habilidade.java
@@ -0,0 +1,13 @@
+package negocio.beans;
+
+public class Habilidade {
+	
+	// Atributos
+	private String nome;
+	private String tipo;
+	private String categoria;
+	private String requisito;
+	private int dificuldade;
+	private int mana;
+	private String descricao;
+}
