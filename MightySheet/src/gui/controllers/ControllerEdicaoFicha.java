2c00af03a3a0db7f99ee737d775e6b8f87eae191
Locodra <48814062+Marcos-Fidelis@users.noreply.github.com>
2019-11-27 23:40:31 -0300

    tela de edicao incompleta

---
+++ MightySheet/src/gui/controllers/ControllerEdicaoFicha.java
@@ -0,0 +1,183 @@
+package gui.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class ControllerEdicaoFicha {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField cargaBasica;
+
+    @FXML
+    private Label ptsAtributos;
+
+    @FXML
+    private TextField esquiva;
+
+    @FXML
+    private Label erro;
+
+    @FXML
+    private ComboBox<?> maoEsquerda;
+
+    @FXML
+    private TextField agilidade;
+
+    @FXML
+    private Button adcAgilidade;
+
+    @FXML
+    private Label qtdHab;
+
+    @FXML
+    private TextField vontade;
+
+    @FXML
+    private Button salvar;
+
+    @FXML
+    private TextField bloqueio;
+
+    @FXML
+    private TextField cargaPesada;
+
+    @FXML
+    private TextField corrida;
+
+    @FXML
+    private ListView<?> skillListC;
+
+    @FXML
+    private TextField ouro;
+
+    @FXML
+    private Button adcForca;
+
+    @FXML
+    private Button voltar;
+
+    @FXML
+    private ListView<?> skilList;
+
+    @FXML
+    private TextField pM;
+
+    @FXML
+    private TextField deslocamento;
+
+    @FXML
+    private ComboBox<?> maoDireita;
+
+    @FXML
+    private TextField inteligencia;
+
+    @FXML
+    private Button adcInteligencia;
+
+    @FXML
+    private TextField pV;
+
+    @FXML
+    private Button adcVontade;
+
+    @FXML
+    private TextField cargaMaxima;
+
+    @FXML
+    private TextField forca;
+
+    @FXML
+    private TextField determinacao;
+
+    @FXML
+    private ComboBox<?> vestimenta;
+
+    @FXML
+    private ComboBox<?> nivel;
+
+    @FXML
+    void upAtributo(ActionEvent event) {
+
+    	
+    	
+    }
+
+    @FXML
+    void acaoBotao(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salvar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void e80808(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Loja(ActionEvent event) {
+
+    }
+
+    @FXML
+    void escolherNivel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void entrega(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert cargaBasica != null : "fx:id=\"cargaBasica\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert ptsAtributos != null : "fx:id=\"ptsAtributos\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert esquiva != null : "fx:id=\"esquiva\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert erro != null : "fx:id=\"erro\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert maoEsquerda != null : "fx:id=\"maoEsquerda\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert agilidade != null : "fx:id=\"agilidade\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert adcAgilidade != null : "fx:id=\"adcAgilidade\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert qtdHab != null : "fx:id=\"qtdHab\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert vontade != null : "fx:id=\"vontade\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert salvar != null : "fx:id=\"salvar\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert bloqueio != null : "fx:id=\"bloqueio\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert cargaPesada != null : "fx:id=\"cargaPesada\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert corrida != null : "fx:id=\"corrida\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert skillListC != null : "fx:id=\"skillListC\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert ouro != null : "fx:id=\"ouro\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert adcForca != null : "fx:id=\"adcForca\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert voltar != null : "fx:id=\"voltar\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert skilList != null : "fx:id=\"skilList\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert pM != null : "fx:id=\"pM\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert deslocamento != null : "fx:id=\"deslocamento\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert maoDireita != null : "fx:id=\"maoDireita\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert inteligencia != null : "fx:id=\"inteligencia\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert adcInteligencia != null : "fx:id=\"adcInteligencia\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert pV != null : "fx:id=\"pV\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert adcVontade != null : "fx:id=\"adcVontade\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert cargaMaxima != null : "fx:id=\"cargaMaxima\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert forca != null : "fx:id=\"forca\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert determinacao != null : "fx:id=\"determinacao\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert vestimenta != null : "fx:id=\"vestimenta\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+        assert nivel != null : "fx:id=\"nivel\" was not injected: check your FXML file 'EdicaoFicha.fxml'.";
+
+    }
+}
